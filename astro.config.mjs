39b938b0f23d600c6e267d1d763ec4322b1bfd62
Tyler Hoyt <tyler.hoyt@hsdesign.dev>
2026-04-07 07:41:58 -0500

    fix: Fixed version issues and basic keystatic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({ mode: "standalone" }),
+  output: "server",
   integrations: [react(), markdoc(), keystatic()],
 });
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,26 +1,19 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, singleton, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: import.meta.env.NODE_ENV === 'dev' ? 'local' : 'github',
-    repo: 'hoyt-steele-design/temtefadezzz'
+    kind: import.meta.env.DEV ? "local" : "github",
+    repo: "hoyt-steele-design/temtefadezzz",
   },
-  collections: {
-    posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+  singletons: {
+    hero: singleton({
+      label: "Hero Section",
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({
-          label: 'Content',
-          options: {
-            image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
-            },
-          },
+        title: fields.text({ label: "Title" }),
+        image: fields.image({
+          label: "Image",
+          directory: "public/images",
+          publicPath: "/images",
         }),
       },
     }),
