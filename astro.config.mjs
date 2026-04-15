f382a74760d4aad5d9f21e077019cf98fc4ac02e
Cole Steele <cole.steele@hsdesign.dev>
2026-04-15 11:30:10 -0500

    fix: styles import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({ mode: "standalone" }),
   output: "server",
   integrations: [markdoc(), keystatic()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
