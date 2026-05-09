7fce533a66036f07ca17c0f57b1c2f2b55ecf989
Cole Steele <cole.steele@hsdesign.dev>
2026-05-09 16:17:10 -0500

    fix: replace deprecated output hybrid with static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: netlify(),
   integrations: [react(), keystatic()],
   vite: {
